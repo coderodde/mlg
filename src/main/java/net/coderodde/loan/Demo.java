62ef02adbc4e7e5593c3545122542c848cc93730
Rodion Efremov <rodion.efremov@helsinki.fi>
2015-04-12 16:11:20 +0300

    N = 20

--- src/main/java/net/coderodde/loan/Demo.java
+++ src/main/java/net/coderodde/loan/Demo.java
@@ -25,7 +25,7 @@ public class Demo {
     /**
      * The amount of nodes in the demo graphs.
      */
-    private static final int LENGTH = 16;
+    private static final int LENGTH = 20;
     
     /**
      * The minimum arc weight.
